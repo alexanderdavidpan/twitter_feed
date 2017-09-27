9d59afa5486b86cdcd342926bfc09bc3670d9654
Alexander David Pan <ap3809@yp.com>
2017-09-27 12:32:19 -0700

    Define one-to-many relationship for users to tweets.

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,8 @@
 class Tweet < ActiveRecord::Base
   validates_presence_of :id, :text, :tweeted_at, :user_id
   validates_uniqueness_of :id
+
+  belongs_to :user
+
+  self.primary_key = "id"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   validates_presence_of :id, :name, :screen_name
   validates_uniqueness_of :id, :screen_name
+
+  has_many :tweets
+
+  self.primary_key = "id"
 end
