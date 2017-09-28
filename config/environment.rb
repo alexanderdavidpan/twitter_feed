ad09e75f227bb0e52c1f7a05b3c53007b6833d39
Alexander David Pan <ap3809@yp.com>
2017-09-27 22:22:42 -0700

    Add twitter gem.
    - Add twitter.yml with sensitive api authentication info to gitignore so it is not public.
    - Developers will need to set up the config/twitter.yml file themselves for security purposes.
    - Initalize twitter client.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,20 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 TwitterFeed::Application.initialize!
+
+# Require twitter lib
+require 'twitter'
+
+# Setup Twitter Client credentials
+env_config = YAML::load_file(File.join(__dir__, 'twitter.yml'))
+
+env_config.each do |key, value|
+  ENV[key] = value
+end
+
+TWITTER_CLIENT = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV['CONSUMER_KEY']
+  config.consumer_secret     = ENV['CONSUMER_SECRET']
+  config.access_token        = ENV['ACCESS_TOKEN']
+  config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+end
